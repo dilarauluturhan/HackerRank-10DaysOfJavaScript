e9054311a286ea7f7ab164cf04cc5f95b9f745c1
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-03-13 11:05:25 +0300

    Day 2 added

---
+++ Day-2/switch.js
@@ -0,0 +1,15 @@
+function getLetter(s){
+     switch(s.charAt(0)){
+        case('a'||'e'||'i'||'o'||'u'):
+            return 'A';
+            
+        case ('b'||'c'||'d'||'f'||'g'):
+            return 'B';
+            
+        case('h'||'j'||'k'||'l'||'m'||'n'):
+            return 'C';
+            
+        case('n'||'p'||'q'||'r'||'s'||'t'||'v'||'w'||'x'||'y'||'z'):
+            return 'D';        
+    }
+}
